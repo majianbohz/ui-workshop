7e52cc5264e905f00f1765669a7f9485155d8b9d
majianbohz <641137800@qq.com>
2024-03-11 11:35:36 +0800

    Add files via upload

---
+++ login.vue
@@ -0,0 +1,33 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import IconEcosystem from "@/components/icons/IconEcosystem.vue";
+</script>
+
+<template>
+  <h2 style="text-align: center; margin-bottom: 3rem"> 垠域网络 </h2>
+  <img style="display: block; height: 8rem; margin: 1rem auto" src="./assets/icon/logo.svg"/>
+  <div style="display: flex; justify-content: center; align-items: center">
+    <img style="height: 1.5rem;  margin: 0.5rem;" src="./assets/icon/i-user.svg"/>
+    <input style="height: 2rem;" type="text" name="fname">
+  </div>
+  <div style="display: flex; justify-content: center; align-items: center">
+    <img style="height: 1.5rem;  margin: 0.5rem;" src="./assets/icon/i-lock.svg"/>
+    <input style="height: 2rem;" type="password" name="lname">
+  </div>
+  <div style="text-align: center">
+    <button style="width: 4rem; margin: 1rem 1rem;"> 登录 </button>
+    <button style="width: 4rem; margin: 1rem 1rem;"> 登出 </button>
+  </div>
+</template>
+
+<style scoped>
+
+ .wrapper {
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+
+    //flex-wrap: wrap;
+  }
+</style>
---
+++ 图片库.vue
@@ -0,0 +1,24 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import IconEcosystem from "@/components/icons/IconEcosystem.vue";
+var list = [1,2,3,4,5,6,7,8,9,10]
+</script>
+
+<template>
+  <div style="display: flex; flex-wrap: wrap">
+    <div v-for="i in list" style="border: solid red 1px; margin: 0.5rem 0.25rem 0.5rem; background-color: #eee">
+      <img style="height: 10rem; width: 10rem" src="http://vip.51haosuan.cn/static/image/hp.png"/>
+      <p style="color: coral; font-size: 2rem">双插盒</p>
+      <p style="color: coral">可选锁舌</p>
+      <p style="color: blueviolet">{{i}}</p>
+    </div>
+  </div>
+  <div style="background-color: #2c3e50; height: 0.2rem; margin: 1rem"></div>
+
+</template>
+
+<style scoped>
+
+
+</style>
